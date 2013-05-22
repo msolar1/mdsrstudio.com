236ac2295d2c35fe86aa8692f4f287d38ed5f383
Mauricio Del Solar <mdsr.pro@gmail.com>
2013-05-22 13:48:02 +0200

    Add work and about static pages
    Add routing, controllers and twig templates for work and about static
    pages. Menu links configured. No css by now.

--- src/mdsr/StudioBundle/Controller/DefaultController.php
+++ src/mdsr/StudioBundle/Controller/DefaultController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name) {
-        return $this->render('mdsrStudioBundle:Default:index.html.twig', array('name' => $name));
-    }
-
     public function workAction() {
         return $this->render('mdsrStudioBundle:Default:work.html.twig');
+    }
+
+    public function aboutAction() {
+        return $this->render('mdsrStudioBundle:Default:about.html.twig');
     }
 }
