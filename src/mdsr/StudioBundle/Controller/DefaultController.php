417e8a5d5f705b532078ea60426a6814607f9df3
Mauricio Del Solar <mdsr.pro@gmail.com>
2013-05-23 14:00:21 +0200

    Add dynamic menu and footer
    Add dynamic menu and change default controller to manage what is the
    off link.
    
    Create a dynamic footer. It's static for now.

--- src/mdsr/StudioBundle/Controller/DefaultController.php
+++ src/mdsr/StudioBundle/Controller/DefaultController.php
@@ -7,10 +7,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function workAction() {
-        return $this->render('mdsrStudioBundle:Default:work.html.twig');
+        return $this->render('mdsrStudioBundle:Default:work.html.twig',
+                             array(
+                                'menuLinkOff' => 'work',
+                                'avaliabilityText' => 'busy'));
     }
 
     public function aboutAction() {
-        return $this->render('mdsrStudioBundle:Default:about.html.twig');
+        return $this->render('mdsrStudioBundle:Default:about.html.twig',
+                             array(
+                                'menuLinkOff' => 'about',
+                                'avaliabilityText' => 'busy'));
     }
 }
