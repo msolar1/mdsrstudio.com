31f855d549684459d38d38be7c735564f56eacd7
Mauricio Del Solar <mdsr.pro@gmail.com>
2013-05-29 17:44:47 +0200

    Fix white space problems in translation catalogs
    Plus delete Acme bundle.

--- src/mdsr/StudioBundle/Controller/DefaultController.php
+++ src/mdsr/StudioBundle/Controller/DefaultController.php
@@ -6,14 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function workAction() {
+    public function workAction()
+    {
         return $this->render('mdsrStudioBundle:Default:work.html.twig',
                              array(
                                 'menuLinkOff' => 'work',
                                 'avaliabilityText' => 'busy'));
     }
 
-    public function trainingAction() {
+    public function trainingAction()
+    {
         $courses = $this->getDoctrine()
                         ->getRepository('mdsrStudioBundle:Course')
                         ->findAll();
@@ -25,14 +27,16 @@ class DefaultController extends Controller
                                 'courses' => $courses));
     }
 
-    public function hireMeAction() {
-        return $this->render('mdsrStudioBundle:Default:hireMe.html.twig', 
+    public function hireAction()
+    {
+        return $this->render('mdsrStudioBundle:Default:hire.html.twig', 
                              array(
                                 'menuLinkOff' => 'hireme',
                                 'avaliabilityText' => 'busy'));
     }
 
-    public function aboutAction() {
+    public function aboutAction()
+    {
         return $this->render('mdsrStudioBundle:Default:about.html.twig',
                              array(
                                 'menuLinkOff' => 'about',
